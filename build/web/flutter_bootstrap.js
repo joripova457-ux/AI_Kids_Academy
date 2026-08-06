02841df457a61f4baf4571451d0de09d6bc871c2
LENOVA <oripovj39@gmail.com>
2026-08-06 07:01:49 +0300

    commit7

--- build/web/flutter_bootstrap.js
+++ build/web/flutter_bootstrap.js
@@ -37,6 +37,6 @@ _flutter.buildConfig = {"engineRevision":"83675ed27633283e7fc296c8bca22e841224c0
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "2519738386" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */
+    serviceWorkerVersion: "68995302" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */
   }
 });
